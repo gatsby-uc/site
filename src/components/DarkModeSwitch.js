126a6b72838b0ba4b349e570610cf4aa3fef4a62
Alex Moon <alex.jared.moon@gmail.com>
2022-02-01 10:06:56 -0800

    fix: darkmode button rendering and add site title

--- src/components/DarkModeSwitch.js
+++ src/components/DarkModeSwitch.js
@@ -4,50 +4,45 @@ import { Switch } from '@headlessui/react';
 import { useDarkMode } from '../libs/hooks';
 
 export default function DarkModeSwitch() {
-  const [enabled, setEnabled] = useDarkMode();
+  const [enabled, setEnabled] = useDarkMode(true);
 
   return (
-    <div className="flex items-center justify-center">
+    <div className="">
       <Switch
         checked={enabled}
         onChange={setEnabled}
-        className={`inline-flex items-center justify-center rounded-lg bg-guc-blue dark:bg-guc-pink w-12 h-12 ${
-          enabled ? 'justify-end' : ''
-        }`}
+        className={`inline-grid grid-cols-1 grid-rows-1 items-center justify-items-center rounded-lg bg-guc-blue dark:bg-guc-pink w-12 h-12 `}
       >
         <span className="sr-only">Enable dark mode</span>
-        {enabled ? (
-          <svg width="24" height="24" fill="none">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M9.353 2.939a1 1 0 01.22 1.08 8 8 0 0010.408 10.408 1 1 0 011.301 1.3A10.003 10.003 0 0112 22C6.477 22 2 17.523 2 12c0-4.207 2.598-7.805 6.273-9.282a1 1 0 011.08.22z"
-              fill={'#000'}
-            />
-          </svg>
-        ) : (
-          <svg width="32" height="32" fill="none">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M16 2a1.4 1.4 0 011.4 1.4v1.4a1.4 1.4 0 11-2.8 0V3.4A1.4 1.4 0 0116 2zM6.1 6.1a1.4 1.4 0 011.98 0l.99.99a1.4 1.4 0 11-1.98 1.98l-.99-.99a1.4 1.4 0 010-1.98zm19.8 0a1.4 1.4 0 010 1.98l-.99.99a1.4 1.4 0 01-1.98-1.98l.99-.99a1.4 1.4 0 011.98 0zM9 16a7 7 0 1114 0 7 7 0 01-14 0zm-7 0a1.4 1.4 0 011.4-1.4h1.4a1.4 1.4 0 110 2.8H3.4A1.4 1.4 0 012 16zm23.8 0a1.4 1.4 0 011.4-1.4h1.4a1.4 1.4 0 110 2.8h-1.4a1.4 1.4 0 01-1.4-1.4zm-2.87 6.93a1.4 1.4 0 011.98 0l.99.99a1.4 1.4 0 01-1.98 1.98l-.99-.99a1.4 1.4 0 010-1.98zm-15.84 0a1.4 1.4 0 011.98 1.98l-.99.99a1.4 1.4 0 01-1.98-1.98l.99-.99zM16 25.8a1.4 1.4 0 011.4 1.4v1.4a1.4 1.4 0 11-2.8 0v-1.4a1.4 1.4 0 011.4-1.4z"
-              fill="url(#paint0_linear)"
-            />
-            <defs>
-              <linearGradient
-                id="paint0_linear"
-                x1="2"
-                y1="2"
-                x2="30"
-                y2="30"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stopColor='#FACC15' />
-                <stop offset="1" stopColor='#FA9D16' />
-              </linearGradient>
-            </defs>
-          </svg>
-        )}
+        <svg width="24" height="24" fill="none" className="hidden dark:block">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.353 2.939a1 1 0 01.22 1.08 8 8 0 0010.408 10.408 1 1 0 011.301 1.3A10.003 10.003 0 0112 22C6.477 22 2 17.523 2 12c0-4.207 2.598-7.805 6.273-9.282a1 1 0 011.08.22z"
+            fill={'#000'}
+          />
+        </svg>
+        <svg width="32" height="32" fill="none" className="block dark:hidden">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M16 2a1.4 1.4 0 011.4 1.4v1.4a1.4 1.4 0 11-2.8 0V3.4A1.4 1.4 0 0116 2zM6.1 6.1a1.4 1.4 0 011.98 0l.99.99a1.4 1.4 0 11-1.98 1.98l-.99-.99a1.4 1.4 0 010-1.98zm19.8 0a1.4 1.4 0 010 1.98l-.99.99a1.4 1.4 0 01-1.98-1.98l.99-.99a1.4 1.4 0 011.98 0zM9 16a7 7 0 1114 0 7 7 0 01-14 0zm-7 0a1.4 1.4 0 011.4-1.4h1.4a1.4 1.4 0 110 2.8H3.4A1.4 1.4 0 012 16zm23.8 0a1.4 1.4 0 011.4-1.4h1.4a1.4 1.4 0 110 2.8h-1.4a1.4 1.4 0 01-1.4-1.4zm-2.87 6.93a1.4 1.4 0 011.98 0l.99.99a1.4 1.4 0 01-1.98 1.98l-.99-.99a1.4 1.4 0 010-1.98zm-15.84 0a1.4 1.4 0 011.98 1.98l-.99.99a1.4 1.4 0 01-1.98-1.98l.99-.99zM16 25.8a1.4 1.4 0 011.4 1.4v1.4a1.4 1.4 0 11-2.8 0v-1.4a1.4 1.4 0 011.4-1.4z"
+            fill="url(#paint0_linear)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="2"
+              y1="2"
+              x2="30"
+              y2="30"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#FACC15" />
+              <stop offset="1" stopColor="#FA9D16" />
+            </linearGradient>
+          </defs>
+        </svg>
       </Switch>
     </div>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Helmet } from 'react-helmet';
 
 import Footer from './Footer';
 import Header from './Header';
@@ -6,6 +7,7 @@ import Header from './Header';
 export default function Layout({ children }) {
   return (
     <>
+      <Helmet title='Gatsby User Collective' />
       <div className="flex min-h-screen flex-col p-4 bg-guc-pink dark:bg-guc-blue transition duration-1000 text-guc-blue dark:text-guc-pink">
         <Header />
         <main className="flex-1 grid my-8">{children}</main>
