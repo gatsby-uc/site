58fbb9de6d684e28b112f0f0571173093bb2eeb8
Alex Moon <alex.jared.moon@gmail.com>
2022-02-01 13:40:25 -0800

    fix: seo and lang tags

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import Header from './Header';
 export default function Layout({ children }) {
   return (
     <>
-      <Helmet title='Gatsby User Collective' />
+      <Helmet title="Gatsby User Collective" meta={"Building and maintaining the the Gatsby ecosystem together."} htmlAttributes={{ lang: 'en-US' }} />
       <div className="flex min-h-screen flex-col p-4 bg-guc-pink dark:bg-guc-blue transition duration-1000 text-guc-blue dark:text-guc-pink">
         <Header />
         <main className="flex-1 grid my-8">{children}</main>
