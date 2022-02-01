5d3ca725a5322c91f36ace6db7a99856885c01ef
Alex Moon <alex.jared.moon@gmail.com>
2022-01-31 16:44:29 -0800

    fix import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import socialIcons from '../components/Icons';
 
 const IndexPage = () => {
