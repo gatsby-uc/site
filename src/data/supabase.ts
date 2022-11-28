6a831fdea526c575e5026af79455e296ae147321
AJ Moon <alex.jared.moon@gmail.com>
2022-11-28 23:16:58 +0000

    fix: v2 changes

--- src/data/supabase.ts
+++ src/data/supabase.ts
@@ -3,14 +3,30 @@ import { getIp } from './ipify';
 
 const { GATSBY_CLOUD = false, BRANCH = null } = process.env;
 
+const SUPABASE_API_URL = process.env.SUPABASE_API_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+const SUPABASE_SECRET_KEY = process.env.SUPABASE_SECRET_KEY;
+
+if (SUPABASE_API_URL == undefined) {
+  throw new Error("SUPABASE_API_URL required")
+}
+
+if (SUPABASE_ANON_KEY == undefined) {
+  throw new Error("SUPABASE_ANON_KEY required")
+}
+
+if (SUPABASE_SECRET_KEY == undefined) {
+  throw new Error("SUPABASE_SECRET_KEY required")
+}
+
 export const publicClient = createClient(
-  process.env.SUPABASE_API_URL,
-  process.env.SUPABASE_ANON_KEY
+  SUPABASE_API_URL,
+  SUPABASE_ANON_KEY
 );
 
 export const serverClient = createClient(
-  process.env.SUPABASE_API_URL,
-  process.env.SUPABASE_SECRET_KEY
+  SUPABASE_API_URL,
+  SUPABASE_SECRET_KEY
 );
 
 type Package = {
@@ -18,10 +34,7 @@ type Package = {
 };
 
 export async function getPackages(supabase: SupabaseClient) {
-  const { data: packages, error } = (await supabase.from('packages').select('name')) as {
-    data: Package[];
-    error: PostgrestError;
-  };
+  const { data: packages, error } = await supabase.from('packages').select('name')
 
   if (error) {
     throw new Error(error.message);
@@ -49,7 +62,7 @@ export type PackageScoreRow = {
 export async function upsertNpmsData(supabase: SupabaseClient, ...rows: NpmsDataRow[]) {
   const { data, error } = await supabase
     .from('npms-data-log')
-    .upsert(rows, { returning: 'minimal' });
+    .upsert(rows);
 
   if (error) {
     throw new Error(error.message);
@@ -61,7 +74,7 @@ export async function upsertNpmsData(supabase: SupabaseClient, ...rows: NpmsData
 export async function upsertPackageScores(supabase: SupabaseClient, ...rows: PackageScoreRow[]) {
   const { data, error } = await supabase
     .from('package-scores')
-    .upsert(rows, { returning: 'minimal' });
+    .upsert(rows);
 
   if (error) {
     throw new Error(error.message);
